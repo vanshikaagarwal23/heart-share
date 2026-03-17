19262db8b6a64d2454f0e4101812630584743c9b
Pari Mishra <mishra.pari.210607@gmail.com>
2026-03-17 20:13:02 +0530

    user.js

---
+++ backend/models/User.js
@@ -0,0 +1,27 @@
+const UserSchema = new mongoose.Schema(
+{
+
+  name:{
+    type:String,
+    required:true
+  },
+
+  email:{
+    type:String,
+    required:true,
+    unique:true
+  },
+
+  password:{
+    type:String,
+    required:true
+  },
+
+  role:{
+    type:String,
+    enum:["donor","ngo","admin"],
+    default:"donor"
+  }
+
+},
+{ timestamps:true });
