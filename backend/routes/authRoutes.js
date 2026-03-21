eac71da0339b1212e37a3a9288a6e12e28a45588
Pari Mishra <mishra.pari.210607@gmail.com>
2026-03-21 10:46:00 +0530

    middleware

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -2,9 +2,40 @@ const express = require("express");
 const router = express.Router();
 
 const { registerUser, loginUser } = require("../controllers/authController");
+const User = require("../models/User");
+const { protect } = require("../middleware/authMiddleware");
 
 
-router.post("/register", registerUser);
-router.post("/login", loginUser);
+// REGISTER
+router.post("/auth/register", registerUser);
+
+
+// LOGIN
+router.post("/auth/login", loginUser);
+
+
+// GET PROFILE
+router.get("/user/profile", protect, async (req, res) => {
+
+  const user = await User.findById(req.user.id).select("-password");
+
+  res.json(user);
+
+});
+
+
+// UPDATE PROFILE
+router.put("/user/update", protect, async (req, res) => {
+
+  const user = await User.findByIdAndUpdate(
+    req.user.id,
+    req.body,
+    { new: true }
+  );
+
+  res.json(user);
+
+});
+
 
 module.exports = router;
