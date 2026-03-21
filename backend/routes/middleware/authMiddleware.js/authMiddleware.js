07be4194bb4931cbf41d306dca2a4f381360ccdc
Pari Mishra <mishra.pari.210607@gmail.com>
2026-03-21 10:49:30 +0530

    commit

--- backend/routes/middleware/authMiddleware.js/authMiddleware.js
+++ backend/routes/middleware/authMiddleware.js/authMiddleware.js
@@ -4,7 +4,7 @@ const jwt = require("jsonwebtoken");
 // AUTHENTICATION MIDDLEWARE
 const protect = (req, res, next) => {
 
-  const token = req.headers.authorization;
+  const token = req.headers.authorization?.split(" ")[1];
 
   if (!token) {
     return res.status(401).json({ message: "Access denied. No token provided." });
