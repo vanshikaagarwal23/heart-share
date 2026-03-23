7019b741179b998288aad7459710f2f635e8e677
Vanshika Agarwal <vanshikaagarwal2304@gmail.com>
2026-03-23 09:45:22 +0530

    donation removed

--- backend/models/Donation.js
+++ backend/models/Donation.js
@@ -1,18 +1,1 @@
 
-// donationj-api 
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-
-const app = express();
-const PORT = process.env.PORT || 5000;
-
-app.use(bodyParser.json());
-
-// Donation Schema
-const donationSchema = new mongoose.Schema({
-  donorName: { type: String, required: true },
-  amount: { type: Number, required: true },
-  message: { type: String },
-  date: { type: Date, default: Date.now }
-});
