99f7c635704536ac48fbbea5386a4069cdbca7fa
Palak Dahale <palakdahale.2511@gmail.com>
2026-03-31 19:03:29 +0530

    Added Dashboard page

---
+++ components/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App =()=> {
+
+  const user ='Harsh'
+  return(
+    <h1>Hello Guys I am {user}</h1>// Curly brackets for displaying values of variable.
+  )
+}
+
+export default App
