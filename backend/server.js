b4509d61b68aaa3ef7ea83730e0e229b0ce0e1bf
Vanshika Agarwal <vanshikaagarwal2304@gmail.com>
2026-04-15 10:32:37 +0530

    Updated project structure

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 
-const routes = require("./routes/authRoutes");
+const routes = require("./src/routes/authRoutes");
 
 const app = express();
 
