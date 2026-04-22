25ef71e8d42aee645ba3c114cb617bcf956c0a56
Vanshika Agarwal <vanshikaagarwal2304@gmail.com>
2026-04-22 14:40:23 +0530

    fixed register issue

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -53,7 +53,7 @@ userSchema.pre("save", async function () {
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
 
-  next();
+  ;
 });
 
 userSchema.methods.comparePassword = async function (enteredPassword) {
